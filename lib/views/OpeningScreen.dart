521c3ea803550a928178e82b82a3dca01c1c6549
Sérgio Vicente Tavuencas <sergiovicente_tavuenc@outlook.com>
2022-10-06 11:27:40 -0300

    Criação da tela inicial

---
+++ lib/views/OpeningScreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../routes.dart';
+
+class OpeningScreen extends StatefulWidget {
+  @override
+  _OpeningViewState createState() => _OpeningViewState();
+}
+
+class _OpeningViewState extends State<OpeningScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: () async => false,
+        child: const Scaffold(
+          backgroundColor: Colors.amberAccent,
+          body: Text('ATVEE'),
+        ));
+  }
+}
