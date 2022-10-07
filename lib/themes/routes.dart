38da155ce62707f58fcbe639f10bb2ddc26e684a
Sérgio Vicente Tavuencas <sergiovicente_tavuenc@outlook.com>
2022-10-07 12:36:54 -0300

    Criação da rota

--- lib/themes/routes.dart
+++ lib/themes/routes.dart
@@ -1,16 +1,21 @@
+import 'package:atvee/screens/user/register_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:atvee/screens/opening/opening_screen.dart';
+
+import '../screens/user/login_screen.dart';
 
 class AppRoutes {
   AppRoutes._();
 
   static const String opening = '/opening';
-  static const String login = '/login';
+  static const String user_login = '/login';
+  static const String user_register = '/register';
 
   static Map<String, WidgetBuilder> define() {
     return {
       opening: (context) => OpeningScreen(),
-      // login:(context) => LoginScreen();
+      user_login: (context) => LoginScreen(),
+      user_register: (context) => RegisterScreen(),
     };
   }
 }
