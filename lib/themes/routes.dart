bad2c424b1509b7c6469278720d492d0aa1c92cc
Sérgio Vicente Tavuencas <sergiovicente_tavuenc@outlook.com>
2022-10-12 11:53:49 -0300

    Criação da rota para a tela de registrar cidades

--- lib/themes/routes.dart
+++ lib/themes/routes.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: constant_identifier_names
-
 import 'package:atvee/screens/home/home_screen.dart';
 import 'package:atvee/screens/user/redefine_password_screen.dart';
+import 'package:atvee/screens/user/register_cities_screen.dart';
 import 'package:atvee/screens/user/register_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:atvee/screens/opening/opening_screen.dart';
@@ -15,6 +15,7 @@ class AppRoutes {
   static const String user_login = '/login';
   static const String user_register = '/register';
   static const String user_redefine_password = '/redefine_password';
+  static const String user_register_cities = '/register_cities';
   static const String home = '/home';
 
   static Map<String, WidgetBuilder> define() {
@@ -23,6 +24,7 @@ class AppRoutes {
       user_login: (context) => LoginScreen(),
       user_register: (context) => RegisterScreen(),
       user_redefine_password: (context) => RedefinePasswordScreen(),
+      user_register_cities: (context) => RegisterCitiesScreen(),
       home: (context) => HomeScreen(),
     };
   }
