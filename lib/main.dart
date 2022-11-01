466b3be1e3f320e70003642245ebf7c87911478e
Sérgio Vicente Tavuencas <sergiovicente_tavuenc@outlook.com>
2022-11-01 12:23:36 -0300

    Definição da classe tema

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:atvee/frontend/screens/user/login_screen.dart';
 import 'package:atvee/frontend/themes/routes.dart';
+import 'package:atvee/frontend/themes/my_theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +18,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ATVEE',
       routes: AppRoutes.define(),
-      home: LoginScreen(),
+      home: const LoginScreen(),
+      theme: MyTheme().appTheme(),
     );
   }
 }
