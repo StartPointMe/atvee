1a5eb9ba0ab3f9ed790633e74259ae6ca6b3db09
Sérgio Vicente Tavuencas <sergiovicente_tavuenc@outlook.com>
2022-11-01 12:31:07 -0300

    Rota deletada

--- lib/frontend/themes/routes.dart
+++ lib/frontend/themes/routes.dart
@@ -3,14 +3,12 @@ import 'package:atvee/frontend/screens/home/home_screen.dart';
 import 'package:atvee/frontend/screens/user/redefine_password_screen.dart';
 import 'package:atvee/frontend/screens/user/register_screen.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:atvee/frontend/screens/opening/opening_screen.dart';
 
 import '../screens/user/login_screen.dart';
 
 class AppRoutes {
   AppRoutes._();
 
-  static const String opening = '/opening';
   static const String user_login = '/login';
   static const String user_register = '/register';
   static const String user_redefine_password = '/redefine_password';
@@ -18,7 +16,6 @@ class AppRoutes {
 
   static Map<String, WidgetBuilder> define() {
     return {
-      opening: (context) => OpeningScreen(),
       user_login: (context) => LoginScreen(),
       user_register: (context) => RegisterScreen(),
       user_redefine_password: (context) => RedefinePasswordScreen(),
