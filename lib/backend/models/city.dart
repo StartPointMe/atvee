2f5bb624a9e789e059d404773fe3aa72e45976fd
Sérgio Vicente Tavuencas <sergiovicente_tavuenc@outlook.com>
2022-11-01 12:25:11 -0300

    Alteracao do model

--- lib/backend/models/city.dart
+++ lib/backend/models/city.dart
@@ -1,6 +1,25 @@
 class City {
-  String name;
-  bool? check;
+  static String get sorocaba => "Sorocaba";
+  static String get votorantim => "Votorantim";
+  static String get saltoDePirapora => "Salto de Pirapora";
+  static String get aracoiabaDaSerra => "Araçoiaba da Serra";
+  static String get ipero => "Iperó";
+  static String get portoFeliz => "Porto Feliz";
+  static String get itu => "Itu";
+  static String get mairinque => "Mairinque";
+  static String get aluminio => "Alumínio";
 
-  City({required this.name, this.check = false});
+  List<String> getCities() {
+    return [
+      sorocaba,
+      votorantim,
+      saltoDePirapora,
+      aracoiabaDaSerra,
+      ipero,
+      portoFeliz,
+      itu,
+      mairinque,
+      aluminio
+    ];
+  }
 }
--- lib/backend/models/occupation.dart
+++ lib/backend/models/occupation.dart
@@ -1,1 +1,9 @@
-enum Occupation { personal, nutricionista, fisioterapeuta }
+class Occupation {
+  static String get personal => "Personal Trainer";
+  static String get nutricionista => "Nutricionista";
+  static String get fisioterapeuta => "Fisioterapeuta";
+
+  List<String> getOccupations() {
+    return [personal, nutricionista, fisioterapeuta];
+  }
+}
