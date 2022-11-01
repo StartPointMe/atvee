eb5d0d3e0ebba76613047039aba4fff86e2a9ecf
Sérgio Vicente Tavuencas <sergiovicente_tavuenc@outlook.com>
2022-11-01 12:26:52 -0300

    Envio de email para verificacao e redefinicao de senha

--- lib/backend/repository/firebase/firebase_auth.dart
+++ lib/backend/repository/firebase/firebase_auth.dart
@@ -6,15 +6,18 @@ class FirebaseAuthentication implements AuthCredential {
   Future<User?> registerUser(String email, String password) async {
     final userCredentials = await firebaseAuth.createUserWithEmailAndPassword(
         email: email, password: password);
-
+    userCredentials.user!.sendEmailVerification();
     return userCredentials.user;
   }
 
   Future<User?> signIn(String email, String password) async {
     final userCredentials = await firebaseAuth.signInWithEmailAndPassword(
         email: email, password: password);
-
     return userCredentials.user;
+  }
+
+  Future<void> resetPassword(String email) async {
+    await firebaseAuth.sendPasswordResetEmail(email: email);
   }
 
   @override
